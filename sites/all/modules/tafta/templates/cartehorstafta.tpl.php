83cd3e142f8450e521e8454fe3f064a97e900a5b
Anas Ben Haj Ali <anas.benhajali@see-tek.com>
2016-07-29 12:00:35 +0100

    filter fixed

--- sites/all/modules/tafta/templates/cartehorstafta.tpl.php
+++ sites/all/modules/tafta/templates/cartehorstafta.tpl.php
@@ -8,9 +8,9 @@
 <div id='list'></div>
 <h3>Veuillez choisir un filtre : </h3>
 <div align="right">
-<button id="cite">Cité</button>
 <button id="ville">Ville</button>
-<button id="pays">Pays</button>
+<button id="region">Région</button>
+<button id="localite">Localité</button>
 </div>
 
 <div id="accordioncontainer">
@@ -19,6 +19,7 @@
     
 </div>
 <!--<script src="http://www.openlayers.org/api/OpenLayers.js"></script>-->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.0-beta.1/jquery-ui.css">
 <script src="OpenLayers.js"></script>
 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js"></script>
@@ -46,13 +47,26 @@
     
     // Define markers as "features" of the vector layer:
     for (let cid of response.rowCid){
-                  jQuery('#accordion').append('<h3>'+ response.rowNomLieu[cid] +'</h3><div>adresse : '+ response.rowAdresse[cid]+'<br/> description : '+ response.rowDescription[cid]+'</div>');
+                  for (let file of response.fileUrl){
+                    if(file['id'] == cid) {
+                      var url = file['fid'];
+                      var yesUrl = true;
+                    }else{
+                      var yesUrl = false;
+                    }
+                  }
+                  if(yesUrl){
+                    jQuery('#accordion').append('<h3>'+ response.rowNomLieu[cid] +'</h3><div><b>Adresse</b> : '+ response.rowAdresse[cid]+'<br/> <b>Description</b> : '+ response.rowDescription[cid]+'<br/><b>Pièce jointe</b> : <a href="sites/default/files/webform/'+ url +'" target="_blank" style="color:red">'+url+'</a></div>');
+                  }else{
+                    jQuery('#accordion').append('<h3>'+ response.rowNomLieu[cid] +'</h3><div><b>Adresse</b> : '+ response.rowAdresse[cid]+'<br/> <b>Description</b> : '+ response.rowDescription[cid]+'</div>');
+                  }
                   lat = parseFloat(response.rowLatitudes[cid]);
                   lon = parseFloat(response.rowLongitudes[cid]);
     var feature = new OpenLayers.Feature.Vector(
              new OpenLayers.Geometry.Point( lon, lat ).transform(epsg4326, projectTo),
             {description:'Nom : '+response.rowNomLieu[cid]+'<br/> Description : '+response.rowDescription[cid]} ,
-            {externalGraphic: 'http://mapping.referata.com/w/images/Blue_marker.png', graphicHeight: 25, graphicWidth: 21, graphicXOffset:-12, graphicYOffset:-25  }
+            //{externalGraphic: 'http://mapping.referata.com/w/images/Blue_marker.png', graphicHeight: 25, graphicWidth: 21, graphicXOffset:-12, graphicYOffset:-25  }
+            {externalGraphic: 'https://www.collectifstoptafta.org/plugins/auto/gis/v4.36.6/lib/leaflet/dist/images/marker-icon.png', graphicHeight: 25, graphicWidth: 21, graphicXOffset:-12, graphicYOffset:-25  }
         );   
     vectorLayer.addFeatures(feature);
   }
@@ -89,26 +103,40 @@
     controls['selector'].activate();
     
     
-            jQuery('#compteur').append('<h2> Vous êtes ' + response.compteur + ' à nous soutenir !</h2>');
+            jQuery('#compteur').append('<h2> '+ response.compteur + ' Lieux Hors TAFTA en France !</h2>');
             jQuery('#accordion').accordion();
           }
         });
         
       // filters
-      jQuery('#cite').on('click',function(){
+      jQuery('#ville').on('click',function(){
         jQuery.ajax({
           url: '/?q=filtercite',
           success: function (response) {
             console.log(response);
             
+            
             jQuery('#accordioncontainer').empty();
             jQuery('#accordioncontainer').append("<div id='accordion'></div>");
             jQuery.each(response.rowCityCid,function( index, values ) {
               var ville = index;
-              var title = jQuery('<h3>'+ index +'</h3><div id="'+index+'"></div>');
+              ville = ville.replace(/ /g,"");
+              var title = jQuery('<h3>'+ index +'</h3><div id="'+ville+'"></div>');
               jQuery('#accordion').append(title);
-              jQuery.each(values,function( index, value ) {  
-                  jQuery('#'+ville).append('Nom du lieu : '+response.rowNomLieu[value]+ '<br/>adresse : '+ response.rowAdresse[value]+'<br/> description : '+ response.rowDescription[value]+'<br/><hr>');
+              jQuery.each(values,function( index, value ) {
+                  for (let file of response.fileUrl){
+                    if(file['id'] == value) {
+                      var url = file['fid'];
+                      var yesUrl = true;
+                    }else{
+                      var yesUrl = false;
+                    }
+                  }
+                  if(yesUrl){
+                    jQuery('#'+ville).append('<b>Nom du lieu</b> : '+response.rowNomLieu[value]+ '<br/><b>Adresse</b> : '+ response.rowAdresse[value]+'<br/> <b>Description</b> : '+ response.rowDescription[value]+'<br/><b>Pièce jointe</b> : <a href="sites/default/files/webform/'+ url +'" target="_blank" style="color:red">'+url+'</a><br/><hr>');
+                  }else{
+                    jQuery('#'+ville).append('<b>Nom du lieu</b> : '+response.rowNomLieu[value]+ '<br/><b>Adresse</b> : '+ response.rowAdresse[value]+'<br/> <b>Description</b> : '+ response.rowDescription[value]+'<br/><hr>');
+                  }
                 });  
             });
                 
@@ -117,31 +145,9 @@
         });  
       });
       
-      jQuery('#ville').on('click',function(){
+      jQuery('#region').on('click',function(){
         jQuery.ajax({
           url: '/?q=filteretat',
-          success: function (response) {
-            console.log(response);
-            jQuery('#accordioncontainer').empty();
-            jQuery('#accordioncontainer').append("<div id='accordion'></div>");
-            jQuery.each(response.rowCityCid,function( index, values ) {
-              var ville = index;
-              var title = jQuery('<h3>'+ index +'</h3><div id="'+index+'"></div>');
-              jQuery('#accordion').append(title);
-              jQuery.each(values,function( index, value ) {  
-                  jQuery('#'+ville).append('Nom du lieu : '+response.rowNomLieu[value]+ '<br/>adresse : '+ response.rowAdresse[value]+'<br/> description : '+ response.rowDescription[value]+'<br/><hr>');
-                });  
-            });
-                
-            
-            jQuery('#accordion').accordion();
-          }
-        });  
-      });
-      
-      jQuery('#pays').on('click',function(){
-        jQuery.ajax({
-          url: '/?q=filterpays',
           success: function (response) {
             console.log(response);
             jQuery('#accordioncontainer').empty();
@@ -151,9 +157,56 @@
               ville = ville.replace(/ /g,"");
               var title = jQuery('<h3>'+ index +'</h3><div id="'+ville+'"></div>');
               jQuery('#accordion').append(title);
+              jQuery.each(values,function( index, value ) {  
+                 for (let file of response.fileUrl){
+                    if(file['id'] == value) {
+                      var url = file['fid'];
+                      var yesUrl = true;
+                    }else{
+                      var yesUrl = false;
+                    }
+                  }
+                  if(yesUrl){
+                    jQuery('#'+ville).append('<b>Nom du lieu</b> : '+response.rowNomLieu[value]+ '<br/><b>Adresse</b> : '+ response.rowAdresse[value]+'<br/> <b>Description</b> : '+ response.rowDescription[value]+'<br/><b>Pièce jointe</b> : <a href="sites/default/files/webform/'+ url +'" target="_blank" style="color:red">'+url+'</a><br/><hr>');
+                  }else{
+                    jQuery('#'+ville).append('<b>Nom du lieu</b> : '+response.rowNomLieu[value]+ '<br/><b>Adresse</b> : '+ response.rowAdresse[value]+'<br/> <b>Description</b> : '+ response.rowDescription[value]+'<br/><hr>');
+                  }
+              });  
+            });
+                
+            
+            jQuery('#accordion').accordion();
+          }
+        });  
+      });
+      
+      jQuery('#localite').on('click',function(){
+        jQuery.ajax({
+          url: '/?q=filterlocalite',
+          success: function (response) {
+            console.log(response);
+            jQuery('#accordioncontainer').empty();
+            jQuery('#accordioncontainer').append("<div id='accordion'></div>");
+            jQuery.each(response.rowCityCid,function( index, values ) {
+              var ville = index;
+              ville = ville.replace(/\//g,"");
+              var title = jQuery('<h3>'+ index +'</h3><div id="'+ville+'"></div>');
+              jQuery('#accordion').append(title);
               jQuery.each(values,function( index, value ) { 
-                  jQuery('#'+ville).append('Nom du lieu : '+response.rowNomLieu[value]+ '<br/>adresse : '+ response.rowAdresse[value]+'<br/> description : '+ response.rowDescription[value]+'<br/><hr>');
-                });  
+                for (let file of response.fileUrl){
+                    if(file['id'] == value) {
+                      var url = file['fid'];
+                      var yesUrl = true;
+                    }else{
+                      var yesUrl = false;
+                    }
+                  }
+                  if(yesUrl){
+                    jQuery('#'+ville).append('<b>Nom du lieu</b> : '+response.rowNomLieu[value]+ '<br/><b>Adresse</b> : '+ response.rowAdresse[value]+'<br/> <b>Description</b> : '+ response.rowDescription[value]+'<br/><b>Pièce jointe</b> : <a href="sites/default/files/webform/'+ url +'" target="_blank" style="color:red">'+url+'</a><br/><hr>');
+                  }else{
+                    jQuery('#'+ville).append('<b>Nom du lieu</b> : '+response.rowNomLieu[value]+ '<br/><b>Adresse</b> : '+ response.rowAdresse[value]+'<br/> <b>Description</b> : '+ response.rowDescription[value]+'<br/><hr>');
+                  }
+              });  
             });
                 
             
