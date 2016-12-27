21e50fde94340195d4f0efda36209cc478bea9b8
Anas Ben Haj Ali <anas.benhajali@see-tek.com>
2016-12-27 11:34:43 +0100

    ok

--- sites/all/modules/tafta/templates/cartehorstafta.tpl.php
+++ sites/all/modules/tafta/templates/cartehorstafta.tpl.php
@@ -8,7 +8,7 @@
 <div id='list'></div>
 <h3>Veuillez choisir un filtre : </h3>
 <div align="right">
-<button id="ville">Ville</button>
+<button id="ville">Commune</button>
 <button id="region">Région</button>
 <button id="localite">Localité</button>
 </div>
@@ -42,32 +42,44 @@
     
     var zoom=5;
     map.setCenter (lonLat, zoom);
-
-    var vectorLayer = new OpenLayers.Layer.Vector("Overlay");
     
+    var vectorLayer = new OpenLayers.Layer.Vector("Overlay");
+ 
     // Define markers as "features" of the vector layer:
-    for (let cid of response.rowCid){
-                  for (let file of response.fileUrl){
-                    if(file['id'] == cid) {
-                      var url = file['fid'];
+    for (var cid of response.rowCid){
+        var yesUrl = false;
+        var iFile = 0 ;
+        var longueur = response.fileUrl.length;  
+                  while((yesUrl == false) && (iFile < longueur) ){
+                    if(response.fileUrl[iFile]['id'] == cid) {
+                      var url = response.fileUrl[iFile]['fid'];
                       var yesUrl = true;
-                    }else{
-                      var yesUrl = false;
                     }
+                    iFile++;
                   }
                   if(yesUrl){
                     jQuery('#accordion').append('<h3>'+ response.rowNomLieu[cid] +'</h3><div><b>Adresse</b> : '+ response.rowAdresse[cid]+'<br/> <b>Description</b> : '+ response.rowDescription[cid]+'<br/><b>Pièce jointe</b> : <a href="sites/default/files/webform/'+ url +'" target="_blank" style="color:red">'+url+'</a></div>');
+                    lat = parseFloat(response.rowLatitudes[cid]);
+                    lon = parseFloat(response.rowLongitudes[cid]);
+                    var feature = new OpenLayers.Feature.Vector(
+                      new OpenLayers.Geometry.Point( lon, lat ).transform(epsg4326, projectTo),
+                      {description:'Nom : '+response.rowNomLieu[cid]+'<br/> Description : '+response.rowDescription[cid]+'<br/><b>Pièce jointe</b> : <a href="sites/default/files/webform/'+ url +'" target="_blank" style="color:red">'+url+'</a>'} ,
+                      //{externalGraphic: 'http://mapping.referata.com/w/images/Blue_marker.png', graphicHeight: 25, graphicWidth: 21, graphicXOffset:-12, graphicYOffset:-25  }
+                      //{externalGraphic: 'https://cdn1.iconfinder.com/data/icons/Map-Markers-Icons-Demo-PNG/128/Map-Marker-Marker-Outside-Azure.png', graphicHeight: 25, graphicWidth: 21, graphicXOffset:-12, graphicYOffset:-25  }
+                      {externalGraphic: 'img/marker_blue.png', graphicHeight: 25, graphicWidth: 21, graphicXOffset:-12, graphicYOffset:-25  }
+                    ); 
                   }else{
-                    jQuery('#accordion').append('<h3>'+ response.rowNomLieu[cid] +'</h3><div><b>Adresse</b> : '+ response.rowAdresse[cid]+'<br/> <b>Description</b> : '+ response.rowDescription[cid]+'</div>');
+                    jQuery('#accordion').append('<h3>'+ response.rowNomLieu[cid] +'</h3><div><b>Type d\'activité</b> : '+ response.rowType[cid]+'<br/><b>Adresse</b> : '+ response.rowAdresse[cid]+'<br/> <b>Description</b> : '+ response.rowDescription[cid]+'</div>');
+                    lat = parseFloat(response.rowLatitudes[cid]);
+                    lon = parseFloat(response.rowLongitudes[cid]);
+                    var feature = new OpenLayers.Feature.Vector(
+                      new OpenLayers.Geometry.Point( lon, lat ).transform(epsg4326, projectTo),
+                     {description:'Nom : '+response.rowNomLieu[cid]+'<br/> Description : '+response.rowDescription[cid]} ,
+                     //{externalGraphic: 'http://mapping.referata.com/w/images/Blue_marker.png', graphicHeight: 25, graphicWidth: 21, graphicXOffset:-12, graphicYOffset:-25  }
+                     {externalGraphic: 'img/marker_blue.png', graphicHeight: 25, graphicWidth: 21, graphicXOffset:-12, graphicYOffset:-25  }
+                    ); 
                   }
-                  lat = parseFloat(response.rowLatitudes[cid]);
-                  lon = parseFloat(response.rowLongitudes[cid]);
-    var feature = new OpenLayers.Feature.Vector(
-             new OpenLayers.Geometry.Point( lon, lat ).transform(epsg4326, projectTo),
-            {description:'Nom : '+response.rowNomLieu[cid]+'<br/> Description : '+response.rowDescription[cid]} ,
-            //{externalGraphic: 'http://mapping.referata.com/w/images/Blue_marker.png', graphicHeight: 25, graphicWidth: 21, graphicXOffset:-12, graphicYOffset:-25  }
-            {externalGraphic: 'https://www.collectifstoptafta.org/plugins/auto/gis/v4.36.6/lib/leaflet/dist/images/marker-icon.png', graphicHeight: 25, graphicWidth: 21, graphicXOffset:-12, graphicYOffset:-25  }
-        );   
+                   
     vectorLayer.addFeatures(feature);
   }
     
@@ -104,7 +116,9 @@
     
     
             jQuery('#compteur').append('<h2> '+ response.compteur + ' Lieux Hors TAFTA en France !</h2>');
-            jQuery('#accordion').accordion();
+            jQuery('#accordion').accordion({
+              heightStyle: "content"
+            });
           }
         });
         
@@ -114,33 +128,36 @@
           url: '/?q=filtercite',
           success: function (response) {
             console.log(response);
-            
-            
             jQuery('#accordioncontainer').empty();
             jQuery('#accordioncontainer').append("<div id='accordion'></div>");
             jQuery.each(response.rowCityCid,function( index, values ) {
               var ville = index;
               ville = ville.replace(/ /g,"");
+              ville = ville.replace(/\'/g,"");
               var title = jQuery('<h3>'+ index +'</h3><div id="'+ville+'"></div>');
               jQuery('#accordion').append(title);
               jQuery.each(values,function( index, value ) {
-                  for (let file of response.fileUrl){
-                    if(file['id'] == value) {
-                      var url = file['fid'];
+                  var yesUrl = false;
+                  var iFile = 0 ;
+                  var longueur = response.fileUrl.length;  
+                  while((yesUrl == false) && (iFile < longueur) ){
+                    if(response.fileUrl[iFile]['id'] == value) {
+                      var url = response.fileUrl[iFile]['fid'];
                       var yesUrl = true;
-                    }else{
-                      var yesUrl = false;
                     }
+                    iFile++;
                   }
                   if(yesUrl){
-                    jQuery('#'+ville).append('<b>Nom du lieu</b> : '+response.rowNomLieu[value]+ '<br/><b>Adresse</b> : '+ response.rowAdresse[value]+'<br/> <b>Description</b> : '+ response.rowDescription[value]+'<br/><b>Pièce jointe</b> : <a href="sites/default/files/webform/'+ url +'" target="_blank" style="color:red">'+url+'</a><br/><hr>');
+                    jQuery('#'+ville).append('<b>Nom du lieu</b> : '+response.rowNomLieu[value]+ '<br/><b>Type d\'activité</b> : '+ response.rowType[value]+ '<br/><b>Adresse</b> : '+ response.rowAdresse[value]+'<br/> <b>Description</b> : '+ response.rowDescription[value]+'<br/><b>Pièce jointe</b> : <a href="sites/default/files/webform/'+ url +'" target="_blank" style="color:red">'+url+'</a><br/><hr>');
                   }else{
-                    jQuery('#'+ville).append('<b>Nom du lieu</b> : '+response.rowNomLieu[value]+ '<br/><b>Adresse</b> : '+ response.rowAdresse[value]+'<br/> <b>Description</b> : '+ response.rowDescription[value]+'<br/><hr>');
+                    jQuery('#'+ville).append('<b>Nom du lieu</b> : '+response.rowNomLieu[value]+ '<br/><b>Type d\'activité</b> : '+ response.rowType[value]+ '<br/><b>Adresse</b> : '+ response.rowAdresse[value]+'<br/> <b>Description</b> : '+ response.rowDescription[value]+'<br/><hr>');
                   }
                 });  
             });
                 
-            jQuery('#accordion').accordion();
+            jQuery('#accordion').accordion({
+              heightStyle: "content"
+            });
           }
         });  
       });
@@ -155,27 +172,32 @@
             jQuery.each(response.rowCityCid,function( index, values ) {
               var ville = index;
               ville = ville.replace(/ /g,"");
+              ville = ville.replace(/\'/g,"");
               var title = jQuery('<h3>'+ index +'</h3><div id="'+ville+'"></div>');
               jQuery('#accordion').append(title);
               jQuery.each(values,function( index, value ) {  
-                 for (let file of response.fileUrl){
-                    if(file['id'] == value) {
-                      var url = file['fid'];
+                  var yesUrl = false;
+                  var iFile = 0 ;
+                  var longueur = response.fileUrl.length;  
+                  while((yesUrl == false) && (iFile < longueur) ){
+                    if(response.fileUrl[iFile]['id'] == value) {
+                      var url = response.fileUrl[iFile]['fid'];
                       var yesUrl = true;
-                    }else{
-                      var yesUrl = false;
                     }
+                    iFile++;
                   }
                   if(yesUrl){
-                    jQuery('#'+ville).append('<b>Nom du lieu</b> : '+response.rowNomLieu[value]+ '<br/><b>Adresse</b> : '+ response.rowAdresse[value]+'<br/> <b>Description</b> : '+ response.rowDescription[value]+'<br/><b>Pièce jointe</b> : <a href="sites/default/files/webform/'+ url +'" target="_blank" style="color:red">'+url+'</a><br/><hr>');
+                    jQuery('#'+ville).append('<b>Nom du lieu</b> : '+response.rowNomLieu[value]+ '<br/><b>Type d\'activité</b> : '+ response.rowType[value]+ '<br/><b>Adresse</b> : '+ response.rowAdresse[value]+'<br/> <b>Description</b> : '+ response.rowDescription[value]+'<br/><b>Pièce jointe</b> : <a href="sites/default/files/webform/'+ url +'" target="_blank" style="color:red">'+url+'</a><br/><hr>');
                   }else{
-                    jQuery('#'+ville).append('<b>Nom du lieu</b> : '+response.rowNomLieu[value]+ '<br/><b>Adresse</b> : '+ response.rowAdresse[value]+'<br/> <b>Description</b> : '+ response.rowDescription[value]+'<br/><hr>');
+                    jQuery('#'+ville).append('<b>Nom du lieu</b> : '+response.rowNomLieu[value]+ '<br/><b>Type d\'activité</b> : '+ response.rowType[value]+ '<br/><b>Adresse</b> : '+ response.rowAdresse[value]+'<br/> <b>Description</b> : '+ response.rowDescription[value]+'<br/><hr>');
                   }
               });  
             });
                 
             
-            jQuery('#accordion').accordion();
+            jQuery('#accordion').accordion({
+              heightStyle: "content"
+            });
           }
         });  
       });
@@ -189,28 +211,33 @@
             jQuery('#accordioncontainer').append("<div id='accordion'></div>");
             jQuery.each(response.rowCityCid,function( index, values ) {
               var ville = index;
+              ville = ville.replace(/ /g,"");
               ville = ville.replace(/\//g,"");
               var title = jQuery('<h3>'+ index +'</h3><div id="'+ville+'"></div>');
               jQuery('#accordion').append(title);
               jQuery.each(values,function( index, value ) { 
-                for (let file of response.fileUrl){
-                    if(file['id'] == value) {
-                      var url = file['fid'];
+                  var yesUrl = false;
+                  var iFile = 0 ;
+                  var longueur = response.fileUrl.length;  
+                  while((yesUrl == false) && (iFile < longueur) ){
+                    if(response.fileUrl[iFile]['id'] == value) {
+                      var url = response.fileUrl[iFile]['fid'];
                       var yesUrl = true;
-                    }else{
-                      var yesUrl = false;
                     }
+                    iFile++;
                   }
                   if(yesUrl){
-                    jQuery('#'+ville).append('<b>Nom du lieu</b> : '+response.rowNomLieu[value]+ '<br/><b>Adresse</b> : '+ response.rowAdresse[value]+'<br/> <b>Description</b> : '+ response.rowDescription[value]+'<br/><b>Pièce jointe</b> : <a href="sites/default/files/webform/'+ url +'" target="_blank" style="color:red">'+url+'</a><br/><hr>');
+                    jQuery('#'+ville).append('<b>Nom du lieu</b> : '+response.rowNomLieu[value]+ '<br/><b>Type d\'activité</b> : '+ response.rowType[value]+ '<br/><b>Adresse</b> : '+ response.rowAdresse[value]+'<br/> <b>Description</b> : '+ response.rowDescription[value]+'<br/><b>Pièce jointe</b> : <a href="sites/default/files/webform/'+ url +'" target="_blank" style="color:red">'+url+'</a><br/><hr>');
                   }else{
-                    jQuery('#'+ville).append('<b>Nom du lieu</b> : '+response.rowNomLieu[value]+ '<br/><b>Adresse</b> : '+ response.rowAdresse[value]+'<br/> <b>Description</b> : '+ response.rowDescription[value]+'<br/><hr>');
+                    jQuery('#'+ville).append('<b>Nom du lieu</b> : '+response.rowNomLieu[value]+ '<br/><b>Type d\'activité</b> : '+ response.rowType[value]+ '<br/><b>Adresse</b> : '+ response.rowAdresse[value]+'<br/> <b>Description</b> : '+ response.rowDescription[value]+'<br/><hr>');
                   }
               });  
             });
                 
             
-            jQuery('#accordion').accordion();
+            jQuery('#accordion').accordion({
+              heightStyle: "content"
+            });
           }
         });  
       });
